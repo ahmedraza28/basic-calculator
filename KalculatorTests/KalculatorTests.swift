e55c0c9ee9a58c5160fd4222125a92573d7ef0eb
ahmedraza28 <88032839+ahmedraza28@users.noreply.github.com>
2023-11-10 22:29:55 +0500

    Added tests

--- KalculatorTests/KalculatorTests.swift
+++ KalculatorTests/KalculatorTests.swift
@@ -1,25 +1,190 @@
+// KalculatorTests.swift
+// KalculatorTests
 //
-//  KalculatorTests.swift
-//  KalculatorTests
-//
-//  Created by Adil Saleem on 31/10/2023.
-//  Copyright © 2023 Jogendra Singh. All rights reserved.
+// Created by Adil Saleem on 31/10/2023.
+// Copyright © 2023 Jogendra Singh. All rights reserved.
 //
 
 import XCTest
+@testable import Kalculator
 
 final class KalculatorTests: XCTestCase {
 
+    private var calculator: Calculator!
+    private var viewController: ViewController!
+    
+    // MARK: - Setup and Teardown
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        calculator = Calculator()
+        super.setUp()
+        
+        // Creating a ViewController for testing
+        viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MainViewController") as? ViewController
+        UIApplication.shared.keyWindow!.rootViewController = viewController
+        _ = viewController.view
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        calculator = nil
+        super.tearDown()
     }
 
-    func testExample() throws {
-        XCTAssert(true)
+    // MARK: - Positive and Negative Test Cases for Inputting Digits
+
+    func test_positive_input_append_Digit() {
+        // Checking if basic operations give the expected result
+        var operation: CalculatorOperation
+        operation = .add
+        XCTAssertEqual(try calculator.inputDigit(operation.rawValue), "0")
+        operation = .subtract
+        XCTAssertEqual(try calculator.inputDigit(operation.rawValue), "0")
+        operation = .multiply
+        XCTAssertEqual(try calculator.inputDigit(operation.rawValue), "0")
+        operation = .divide
+        XCTAssertEqual(try calculator.inputDigit(operation.rawValue), "0")
+
+        // Checking if digits are correctly appended
+        var input = "1"
+        XCTAssertEqual(try calculator.inputDigit(input), "1")
+        input = "2"
+        XCTAssertEqual(try calculator.inputDigit(input), "12")
     }
 
+    func test_negative_inputDigit() {
+        // Making sure invalid inputs throw errors
+        var input = "ABC"
+        XCTAssertThrowsError(try calculator.inputDigit(input))
+        input = "%"
+        XCTAssertThrowsError(try calculator.inputDigit(input))
+    }
+
+    func test_negative_appendDigit() {
+        // Making sure invalid inputs throw errors
+        var input = "@"
+        XCTAssertThrowsError(try calculator.appendDigit(input))
+        input = "#"
+        XCTAssertThrowsError(try calculator.appendDigit(input))
+    }
+
+    // MARK: - Positive and Negative Test Cases for Basic Operations
+
+    func test_positive_addOperation() {
+        // Making sure addition works as expected
+        // ...
+    }
+
+    func test_negative_addOperation() {
+        // Making sure addition fails for incorrect inputs
+        // ...
+    }
+
+    func test_positive_subtractOperation() {
+        // Making sure subtraction works as expected
+        var num1 = "4"
+        var num2 = "3"
+        var operation: CalculatorOperation
+        operation = .subtract
+        
+        num1 = try! calculator.inputDigit(num1)
+        _ = try! calculator.inputDigit(operation.rawValue)
+        num2 = try! calculator.inputDigit(num2)
+        
+        operation = .equals
+        XCTAssertEqual(try calculator.inputDigit(operation.rawValue), "1")
+    }
+
+    func test_negative_subtractOperation() {
+        // Making sure subtraction fails for incorrect inputs
+        var num1 = "7"
+        var num2 = "3"
+        var operation: CalculatorOperation
+        operation = .subtract
+        
+        num1 = try! calculator.inputDigit(num1)
+        _ = try! calculator.inputDigit(operation.rawValue)
+        num2 = try! calculator.inputDigit(num2)
+        
+        operation = .equals
+        XCTAssertNotEqual(try calculator.inputDigit(operation.rawValue), "5")
+    }
+
+    func test_positive_multiplyOperation() {
+        // Making sure multiplication works as expected
+        var num1 = "5"
+        var num2 = "6"
+        var operation: CalculatorOperation
+        operation = .multiply
+        
+        num1 = try! calculator.inputDigit(num1)
+        _ = try! calculator.inputDigit(operation.rawValue)
+        num2 = try! calculator.inputDigit(num2)
+        
+        operation = .equals
+        XCTAssertEqual(try calculator.inputDigit(operation.rawValue), "30")
+    }
+
+    func test_negative_multiplyOperation() {
+        // Making sure multiplication fails for incorrect inputs
+        var num1 = "3"
+        var num2 = "5"
+        var operation: CalculatorOperation
+        operation = .multiply
+        
+        num1 = try! calculator.inputDigit(num1)
+        _ = try! calculator.inputDigit(operation.rawValue)
+        num2 = try! calculator.inputDigit(num2)
+        
+        operation = .equals
+        XCTAssertNotEqual(try calculator.inputDigit(operation.rawValue), "35")
+    }
+
+    func test_positive_divideOperation() {
+        // Making sure division works as expected
+        var num1 = "8"
+        var num2 = "2"
+        var operation: CalculatorOperation
+        operation = .divide
+        
+        num1 = try! calculator.inputDigit(num1)
+        _ = try! calculator.inputDigit(operation.rawValue)
+        num2 = try! calculator.inputDigit(num2)
+        
+        operation = .equals
+        XCTAssertEqual(try calculator.inputDigit(operation.rawValue), "4")
+    }
+
+    func test_negative_divideOperation() {
+        // Making sure division fails for incorrect inputs
+        var num1 = "8"
+        var num2 = "0"
+        var operation: CalculatorOperation
+        operation = .divide
+        
+        num1 = try! calculator.inputDigit(num1)
+        _ = try! calculator.inputDigit(operation.rawValue)
+        num2 = try! calculator.inputDigit(num2)
+        
+        operation = .equals
+        XCTAssertThrowsError(try calculator.inputDigit(operation.rawValue))
+    }
+
+    // MARK: - Test Cases for Clearing
+
+    func test_clear() {
+        // Testing if the calculator can be cleared
+        // ...
+    }
+
+    // MARK: - Test Cases for ViewController Class
+
+    func test_VC_inputDigit() {
+        // Testing if ViewController's inputDigit works as expected
+        // ...
+    }
+
+    func test_VC_clear() {
+        // Testing if ViewController's clear function resets the screen
+        // ...
+    }
 }
--- KalculatorUITests/KalculatorUITests.swift
+++ KalculatorUITests/KalculatorUITests.swift
@@ -1,6 +1,5 @@
-//
-//  KalculatorUITests.swift
-//  KalculatorUITests
+//  CalculatorUITests.swift
+//  CalculatorUITests
 //
 //  Created by Adil Saleem on 31/10/2023.
 //  Copyright © 2023 Jogendra Singh. All rights reserved.
@@ -8,34 +7,118 @@
 
 import XCTest
 
-final class KalculatorUITests: XCTestCase {
+final class CalculatorUITests: XCTestCase {
 
-    var app:XCUIApplication?
+    private var app: XCUIApplication!
     
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        app = XCUIApplication()
         app.launch()
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        app = nil
     }
 
-    func testExample() throws {
+    // Test Cases for checking the existence of all the Buttons
+    func testButtonsExistence() {
+        XCTAssertTrue(app.buttons["C"].exists)
         
-        // Get all the buttons
-        if let buttons = app?.buttons{
-            XCTAssert(true)
-        }
-        
-        
-    }
+        XCTAssertTrue(app.buttons["+"].exists)
+        XCTAssertTrue(app.buttons["-"].exists)
+        XCTAssertTrue(app.buttons["*"].exists)
+        XCTAssertTrue(app.buttons["/"].exists)
+        XCTAssertTrue(app.buttons["="].exists)
 
+        XCTAssertTrue(app.buttons["0"].exists)
+        XCTAssertTrue(app.buttons["1"].exists)
+        XCTAssertTrue(app.buttons["2"].exists)
+        XCTAssertTrue(app.buttons["3"].exists)
+        XCTAssertTrue(app.buttons["4"].exists)
+        XCTAssertTrue(app.buttons["5"].exists)
+        XCTAssertTrue(app.buttons["6"].exists)
+        XCTAssertTrue(app.buttons["7"].exists)
+        XCTAssertTrue(app.buttons["8"].exists)
+        XCTAssertTrue(app.buttons["9"].exists)
+    }
+    
+    // Test Cases for checking the Result Screen
+    func testResultScreen() {
+        var digitButton = "5"
+        app.buttons[digitButton].tap()
+        XCTAssertEqual(app.staticTexts["Result"].label, digitButton)
+        
+        digitButton = "8"
+        app.buttons[digitButton].tap()
+        XCTAssertEqual(app.staticTexts["Result"].label, "58")
+        
+        digitButton = "+"
+        app.buttons[digitButton].tap()
+        XCTAssertEqual(app.staticTexts["Result"].label, "58")
+        
+        digitButton = "3"
+        app.buttons[digitButton].tap()
+        XCTAssertEqual(app.staticTexts["Result"].label, "3")
+        
+        digitButton = "2"
+        app.buttons[digitButton].tap()
+        XCTAssertEqual(app.staticTexts["Result"].label, "32")
+        
+        digitButton = "="
+        app.buttons[digitButton].tap()
+        XCTAssertEqual(app.staticTexts["Result"].label, "90")
+        
+        digitButton = "C"
+        app.buttons[digitButton].tap()
+        XCTAssertEqual(app.staticTexts["Result"].label, "0")
+    }
+    
+    // Test Cases for checking the Calculated Output
+    func testCalculatedOutput() {
+        app.buttons["2"].tap()
+        app.buttons["3"].tap()
+        app.buttons["/"].tap()
+        app.buttons["4"].tap()
+        app.buttons["="].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "5")
+        
+        app.buttons["*"].tap()
+        app.buttons["7"].tap()
+        app.buttons["8"].tap()
+        app.buttons["="].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "390")
+        
+        app.buttons["+"].tap()
+        app.buttons["9"].tap()
+        app.buttons["2"].tap()
+        app.buttons["="].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "482")
+        
+        app.buttons["-"].tap()
+        app.buttons["6"].tap()
+        app.buttons["0"].tap()
+        app.buttons["="].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "422")
+    }
+    
+    // Test Case for checking Error Detection and Handling
+    func testErrorDetectionAndHandling() {
+        app.buttons["1"].tap()
+        app.buttons["6"].tap()
+        app.buttons["/"].tap()
+        app.buttons["0"].tap()
+        app.buttons["="].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "Err")
+        
+        app.buttons["C"].tap()
+        
+        XCTAssertEqual(app.staticTexts["Result"].label, "0")
+    }
 }
